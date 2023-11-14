23dadda9083cac3313da6fd1a3ca8fc501178c5e
@cipriano.johnson69 <cipriano.johson69@gmail.com>
2023-11-14 01:47:29 -0300

    Ajustes no Main.java.

--- src/br/ufrn/imd/abb/Main.java
+++ src/br/ufrn/imd/abb/Main.java
@@ -1,20 +1,14 @@
 package br.ufrn.imd.abb;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.util.Scanner;
+import br.ufrn.imd.abb.ArvoresABB;
+import br.ufrn.imd.abb.No;
 
 public class Main {
 	public static void main(String[] args) throws FileNotFoundException {
 
 		ArvoresABB a = new ArvoresABB();
-
-//		a.inserir(30);  
-//		a.inserir(40);
-//		a.inserir(60);
-//		a.inserir(50);
-//			
-//		System.out.println(a.posicao(a.raiz, 40, 0));
 		
 		String path = System.getProperty("user.dir")+"/data/arquivo1.txt";
 		int elemento = 0;
@@ -47,42 +41,31 @@ public class Main {
             	}
             }  else if(linhaComandos.equals("PREORDEM")) {
             	a.preOrdem(a.raiz);
-            	System.out.println();
-            	
+            	System.out.println();	
             }  else if(linhaComandos.equals("MEDIANA")) {
             	noPrint = a.mediana();
                 System.out.println(noPrint.valor);       
             } else {
             	String[] comandos = linhaComandos.split(" ");
-            	
               if (comandos[0].equals("ENESIMO")) {
               	noPrint = a.enesimoElemento(a.raiz, Integer.parseInt(comandos[1]));
               	System.out.println(noPrint.valor);
-              	
               } else if (comandos[0].equals("POSICAO")) {
-                  System.out.println(a.posicao(a.raiz, Integer.parseInt(comandos[1]), 0));
-                  
+                System.out.println(a.posicao(a.raiz, Integer.parseInt(comandos[1]), 0));
               } else if (comandos[0].equals("MEDIA")) {
-            	a.imprimirTracos(a.raiz, 0, 30);
-              	System.out.println(a.media(Integer.parseInt(comandos[1])));
-              	
+            	System.out.println(a.media(Integer.parseInt(comandos[1])));
               } else if (comandos[0].equals("IMPRIMA")) {
                   if (comandos[1].equals("1")) {
                   	a.imprimirTracos(a.raiz, 0, 30);
-                  	
                   } else if (comandos[1].equals("2")) {
                   	System.out.println(a.imprimirParenteses(a.raiz));
-                  	
                   }
               } else if (comandos[0].equals("INSIRA")) {
                   a.inserir(Integer.parseInt(comandos[1]));
-                  
               } else if (comandos[0].equals("REMOVA")) {
                   a.delete(Integer.parseInt(comandos[1]));
-                  
               } else if (comandos[0].equals("BUSCAR")) {
                   noPrint = a.busca(a.raiz, Integer.parseInt(comandos[1]));
-                  
                   if(noPrint == null) {
                   	System.out.println("Chave não encontrada");
                   } else {
@@ -92,49 +75,6 @@ public class Main {
             } 
 
         }
-		
-//		a.iniciar();
-
-//		System.out.println("Pré Ordem:");
-//		a.preOrdem(a.raiz);
-//		System.out.println(" ");
-//		System.out.println("Simétrica:");
-//		a.simetrica(a.raiz);
-//		System.out.println(" ");
-//
-//
-//		System.out.println("Pós Ordem:");
-//		a.posOrdem(a.raiz);
-//		System.out.println(" ");
-//
-//		System.out.println("Pré Ordem Iterativa:");
-//		a.preOrdemIterativa(a.raiz);
-//		System.out.println(" ");
-//		
-//		System.out.println("Pós Ordem Iterativa:");
-//		a.posOrdemIterativa(a.raiz);
-//		System.out.println(" ");
-		 
-//		CHEIA
-//		COMPLETA
-//		ENESIMO 3
-//		INSIRA 36
-//		CHEIA
-//		PREORDEM
-//		IMPRIMA 1
-//		IMPRIMA 2
-//		REMOVA 50
-//		INSIRA 15
-//		INSIRA 39
-//		REMOVA 32
-//		POSICAO 15
-//		INSIRA 39
-//		ENESIMO 5
-//		MEDIANA
-//		MEDIA 20
-//		BUSCAR 36
-//		INSIRA 25
-//		MEDIANA
-		
-		}
+		scanner.close();
+	}
 }
